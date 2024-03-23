d9868594a92fd4b9f61b167cc768025abcf43a9e
WendyKariuki <phaminuswendy@gmail.com>
2024-03-23 23:04:57 +0300

    Functions 2 lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun()); 
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork()); 
+console.log(mondayWork("work on a project")); 
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!"); 
+console.log(encouragingPromptFunction("awesome")); 
+console.log(encouragingPromptFunction()); 
+
+const sparklingPromptFunction = wrapAdjective("~*~"); 
+console.log(sparklingPromptFunction("fantastic")); 
+console.log(sparklingPromptFunction());
+
